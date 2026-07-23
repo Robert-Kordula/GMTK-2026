a4897ea0b0d67b4349008aec235e0bf9a6a72eca
Bobby Kordula <bobbykordula@gmail.com>
2026-07-23 15:36:03 +0100

    Added BiteHitbox to Player scene This will represent the players default attack (currently always visible) Added animations to Player and rotations to BiteHitbox to indicate position the player is facing

--- Player/player.gd
+++ Player/player.gd
@@ -1,6 +1,12 @@
 extends CharacterBody2D
 
-@export var speed = 300.0
+@export var speed:float = 300.0
+
+@onready var animated_sprite:AnimatedSprite2D = $SeanSprite
+@onready var bite_hitbox:Area2D = $BiteHitbox
+
+func _ready():
+	animated_sprite.play('right')
 
 func _physics_process(_delta):
 
@@ -10,9 +16,30 @@ func _physics_process(_delta):
 
 # Get the input direction and handle the movement.
 func handle_movement_keys():
-	var x_direction = Input.get_axis("move_left", "move_right")
-	var y_direction = Input.get_axis("move_up", "move_down")
+	var direction:= Input.get_vector('move_left', 'move_right', 'move_up', 'move_down')
 
-	var direction:= Vector2(x_direction, y_direction)
+	if direction.x > 0:
+		animated_sprite.play('right')
+	elif direction.x < 0:
+		animated_sprite.play('left')
 
-	velocity = direction.normalized() * speed
+	if direction.x != 0 && direction.y != 0:
+		var degree_multiplier = sign(direction.x)
+
+		if direction.y < 0:
+			animated_sprite.rotation_degrees = degree_multiplier * -45
+		elif direction.y > 0:
+			animated_sprite.rotation_degrees = degree_multiplier * 45
+	elif direction.x != 0 && direction.y == 0:
+		animated_sprite.rotation_degrees = 0
+	elif direction.y > 0:
+		animated_sprite.play('down')
+		animated_sprite.rotation_degrees = 0
+	elif direction.y < 0:
+		animated_sprite.play('up')
+		animated_sprite.rotation_degrees = 0
+
+	if direction != Vector2.ZERO:
+		bite_hitbox.look_at(direction + global_position)
+
+	velocity = direction * speed
